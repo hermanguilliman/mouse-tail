8de685e9777f05c26be13fdd2577c5e995a97d43
Herman Guilliman <glmn.dev@protonmail.com>
2022-11-23 12:50:38 +0300

    made external config.ini file loading

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 small application to display mouse coordinates next to the cursor
 
-you can easily change font, sizes and color
+you can easily change font, sizes and color in config.ini file
--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 from tkinter import *
 from ctypes import windll, Structure, c_long, byref
+import configparser
+from dataclasses import dataclass
 
-width = 100
-height = 50
-refresh_rate = 1
-label_color = '#F2D632'
-label_font = ('Terminal', 8, 'bold')
-root_transparent = 1
+@dataclass
+class Settings:
+    width: int
+    height: int
+    refresh_rate: int
+    font_family: str
+    font_size: int
+    font_weight: str
+    font_color: str
+    root_transparent: float
 
+
+def load_config(path: str):
+    config = configparser.ConfigParser()
+    config.read(path)
+    config = Settings(**config['settings'])
+    return config
 
 class POINT(Structure):
     _fields_ = [("x", c_long), ("y", c_long)]
@@ -21,25 +33,27 @@ def queryMousePosition():
 
 def main():
     root = Tk()
+    config = load_config('config.ini')
     root.overrideredirect(True)
     root.attributes('-topmost',True)
     root.wm_attributes("-disabled", True)
     root.wm_attributes("-transparentcolor", "#FF00FF")
-    root.wm_attributes("-alpha", root_transparent)
-    root.configure(width=width, height=height, background="#FF00FF")    
-    root.geometry(f"{width}x{height}")
+    root.wm_attributes("-alpha", config.root_transparent)
+    root.configure(width=config.width, height=config.height, background="#FF00FF")    
+    root.geometry(f"{config.width}x{config.height}")
     
-    label = Label(root, background='#FF00FF', foreground=label_color, font=label_font, justify="left")
+    label = Label(root, background='#FF00FF', foreground=config.font_color, font=(config.font_family, config.font_size, config.font_weight), justify="left")
     label.pack(fill="both", expand=True)
     
     def update():
         mouse_x, mouse_y = queryMousePosition()
         label['text'] = f'x:{mouse_x} y:{mouse_y}'
-        root.geometry(f'{width}x{height}+{mouse_x+5}+{mouse_y+5}')
-        root.after(refresh_rate, update)
+        root.geometry(f'{config.width}x{config.height}+{mouse_x+5}+{mouse_y+5}')
+        root.after(config.refresh_rate, update)
 
     update()
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
